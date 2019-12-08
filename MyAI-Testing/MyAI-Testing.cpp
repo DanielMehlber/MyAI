cffaaf039e53e911ddadb6a077b213a189f03011
Daniel <daniel.mehlber@gmail.com>
2019-12-08 14:44:49 +0100

    read-write finished. Newest version of BinarIO

--- MyAI-Testing/MyAI-Testing.cpp
+++ MyAI-Testing/MyAI-Testing.cpp
@@ -9,16 +9,20 @@ int main()
 
 	print("Started...");
 	myai::cnn::CNN cnn{
-		126,
-		5,
+		126 * 126,
+		1000,
 		3,
-		5,
-		126
+		1000,
+		126 * 126
 	};
 
-	cnn.save("C://Users/danie/Documents/NN.cnn");
-	cnn.load("C://Users/danie/Documents/NN.cnn");
-
+	try {
+		cnn.save("C://Users/danie/Documents/NN.cnn");
+		cnn.load("C://Users/danie/Documents/NN.cnn");
+	}
+	catch (std::exception & e) {
+		println("ERROR occured: " << e.what());
+	}
 	return 0;
 }
 
